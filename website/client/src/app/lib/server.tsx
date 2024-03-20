ba3e7ebc0f210b62cde0ce5ac3e8923658da50f2
Marrollar <n@a>
2024-03-20 15:52:01 -0400

    Changed endpoints slightly

--- website/client/src/app/lib/server.tsx
+++ website/client/src/app/lib/server.tsx
@@ -1,6 +1,6 @@
 export async function fetchStations() {
     try {
-        const stations_resp = await fetch("http://127.0.0.1:8080/stations", { method: "GET", cache: "no-store" }).then(x => x.json())
+        const stations_resp = await fetch(`${process.env.ENDPOINT_ROOT}/rs/stations`, { method: "GET", cache: "no-store" }).then(x => x.json())
         return stations_resp
     } catch (error) {
         console.error("Error retrieving stations: ", error)
@@ -10,7 +10,7 @@ export async function fetchStations() {
 
 export async function fetchLatestRecords() {
     try {
-        const records_resp = await fetch("http://127.0.0.1:8080/lastrecords", { method: "GET", cache: "no-store" }).then(x => x.json())
+        const records_resp = await fetch(`${process.env.ENDPOINT_ROOT}/rs/lastrecords`, { method: "GET", cache: "no-store" }).then(x => x.json())
         return records_resp
     } catch (error) {
         console.error("Error retrieving latest records: ", error)
