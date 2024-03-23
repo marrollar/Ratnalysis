1d5c432ed5a4f1963c1414bf8cbe50856ec160f8
Marrollar <n@a>
2024-03-23 02:21:33 -0400

    Moved an api call to the central file

--- website/client/src/app/charts/page.tsx
+++ website/client/src/app/charts/page.tsx
@@ -1,14 +1,5 @@
 import ChartsPage from "@/components/ChartsPage";
-
-async function fetchStations() {
-    try {
-        const stations_resp = await fetch("http://127.0.0.1:8080/rs/stations", { method: "GET", cache: "no-store" }).then(x => x.json())
-        return stations_resp
-    } catch (error) {
-        console.error("Error retrieving stations: ", error)
-        return null
-    }
-}
+import { fetchStations } from "../lib/server";
 
 export default async function Page() {
     const stations_resp = await fetchStations()
--- website/client/src/app/lib/client.tsx
+++ website/client/src/app/lib/client.tsx
@@ -2,7 +2,7 @@ import useSWR from "swr"
 
 async function fetcher(url) {
     try {
-        const resp = await fetch(`http://127.0.0.1:8080${url}`, { method: "GET", cache: "no-store" }).then(x => x.json())
+        const resp = await fetch(`${process.env.NEXT_PUBLIC_ENDPOINT_ROOT}${url}`, { method: "GET", cache: "no-store" }).then(x => x.json())
         return resp
     } catch (error) {
         console.error("Error with request")
