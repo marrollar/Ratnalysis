02a222440d5203a190ef1613db79cf96ba2e43ec
Marrollar <n@a>
2024-04-04 14:39:16 -0400

    Added loading checks to make website more responsive to laggy loading which sort of dont work

--- website/client/src/components/Navbar.tsx
+++ website/client/src/components/Navbar.tsx
@@ -1,11 +1,40 @@
 "use client";
 
+import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import clsx from 'clsx';
+
+import { Router } from "next/router";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
     const pathname = usePathname();
+    const [loading, setLoading] = useState(false);
+    const [whichIsLoading, setWhichIsLoading] = useState("/")
+
+    // TODO: no effect
+    useEffect(() => {
+        const handleRouteChangeStart = () => {
+            console.log("changing")
+            setLoading(true)
+        };
+        const handleRouteChangeComplete = () => {
+            console.log("finished changing")
+            setLoading(false)
+        };
+
+        Router.events.on("routeChangeStart", handleRouteChangeStart);
+        Router.events.on("routeChangeComplete", handleRouteChangeComplete);
+
+        return () => {
+            Router.events.off("routeChangeStart", handleRouteChangeStart);
+            Router.events.off("routeChangeComplete", handleRouteChangeComplete);
+        };
+    }, []);
+
+    const handleLinkClick = (e: React.MouseEvent, path: string) => {
+        setWhichIsLoading(path)
+    }
 
     return (
         <div className='flex w-full'>
@@ -13,35 +42,38 @@ const Navbar = () => {
                 <div className="container mx-auto">
                     <ul className="flex justify-center items-center space-x-4 pt-0.5">
                         <li>
-                            <Link href="/">
+                            <Link href="/" onClick={(e) => handleLinkClick(e, "/")}>
                                 <div className={clsx(
                                     "font-bold text-white hover:text-gray-300 border-b-4 hover:border-gray-500",
                                     {
                                         "border-gray-500 text-gray-300": pathname === "/",
                                         "border-transparent": pathname !== "/",
+                                        // "border-red-500 text-red-500": (whichIsLoading === "/" && pathname !== "/")
                                     }
                                 )}
                                 > Home </div>
                             </Link>
                         </li>
                         <li>
-                            <Link href="/geomap">
+                            <Link href="/geomap" onClick={(e) => handleLinkClick(e, "/geomap")}>
                                 <div className={clsx(
                                     "font-bold text-white hover:text-gray-300 border-b-4 hover:border-gray-500",
                                     {
                                         "border-gray-500 text-gray-300": pathname === "/geomap",
                                         "border-transparent": pathname !== "/geomap",
+                                        // "border-red-500 text-red-500": (whichIsLoading === "/geomap" && pathname !== "/geomap")
                                     }
                                 )}> GeoMap </div>
                             </Link>
                         </li>
                         <li>
-                            <Link href="/charts">
+                            <Link href="/charts" onClick={(e) => handleLinkClick(e, "/charts")}>
                                 <div className={clsx(
                                     "font-bold text-white hover:text-gray-300 border-b-4 hover:border-gray-500",
                                     {
                                         "border-gray-500 text-gray-300": pathname === "/charts",
                                         "border-transparent": pathname !== "/charts",
+                                        // "border-red-500 text-red-500": (whichIsLoading === "/charts" && pathname !== "/charts")
                                     }
                                 )}> Charts</div>
                             </Link>
